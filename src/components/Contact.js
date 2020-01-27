897547fb1e51d1b0c938beec5e58d0a5369b4758
emi.luciani18@gmail.com <59806361+emiluce@users.noreply.github.com>
2020-01-27 16:06:09 +0100

    finished exercise

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,7 +7,7 @@ function Contact(props) {
 
   return (
     <div className="Contact">
-      <img className="avatar" src="https://i.pinimg.com/originals/5c/06/68/5c0668716bad0772cd6e64e300477300.jpg" alt="" />
+      <img className="avatar" src="http://randomuser.me/api/portraits/women/10.jpg" alt="" />
       <div>
         <h4 className="name">{name}</h4>
         <p className="status">{online ? "Online" : "Offline"}</p>
